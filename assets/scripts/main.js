84e06616333c7feadf5d62f5abc1b9c45abb4a5f
Casey Jardin <casey@iconologic.com>
2015-10-12 14:28:40 -0400

    main.js added

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+$(function () {
+	
+	$('#mailingList').hover(
+		function(){ $('.diagonal, #mailingList').addClass('hover') },
+		function(){ $('.diagonal, #mailingList').removeClass('hover') }
+	)
+
+});
